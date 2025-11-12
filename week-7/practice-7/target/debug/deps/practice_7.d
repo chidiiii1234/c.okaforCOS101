a91e1b7822b8c7e5867484933b29bd7d8c94aa1c
unknown <cmo.okafor@gmail.com>
2025-11-12 15:52:54 -0800

    (week 7 commit)

---
+++ week-7/practice-7/target/debug/deps/practice_7.d
@@ -0,0 +1,5 @@
+C:\Users\DELL\Documents\CSC101\c.okaforCOS101\week-7\practice-7\target\debug\deps\practice_7.d: src\main.rs
+
+C:\Users\DELL\Documents\CSC101\c.okaforCOS101\week-7\practice-7\target\debug\deps\practice_7.exe: src\main.rs
+
+src\main.rs:
---
+++ week-7/practice-8/target/debug/practice-8.d
@@ -0,0 +1,1 @@
+C:\Users\DELL\Documents\CSC101\c.okaforCOS101\week-7\practice-8\target\debug\practice-8.exe: C:\Users\DELL\Documents\CSC101\c.okaforCOS101\week-7\practice-8\src\main.rs
